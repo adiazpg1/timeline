8ecfe7974ee72b6c376a76a3b2df8f84a178487b
Ariel Diaz <adiaz@perceptiongrp.com>
2018-03-16 12:57:37 -0300

    Linetime

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer } from '@angular/core';
 import { SharepointService } from '../../services/sharepoint.service';
 import { Noticias } from '../../interfaces/noticias';
 import { Subscription } from 'rxjs/Subscription';
@@ -9,28 +9,22 @@ import { Months } from '../../interfaces/months';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-
-  _MESES: string[] = [
-    'Enero',
-    'Febrero',
-    'Marzo',
-    'Abril',
-    'Mayo',
-    'Junio',
-    'Julio',
-    'Agosto',
-    'Septiembre',
-    'Noviembre',
-    'Diciembre'
-  ];
   data: Noticias[] = [];
   dateToday: Date = new Date();
   mesSelected: Months;
   daysLine: any[] = [];
 
-  constructor(private _sharepointService: SharepointService) {
+
+  itemSelected: number;
+  contentSelected: boolean;
+
+  constructor(private _sharepointService: SharepointService,
+    private el: ElementRef,
+    private renderer: Renderer) {
+
     this._sharepointService.getData().subscribe(
       (param: any) => {
         this.data = param.filter(x => {
@@ -39,6 +33,7 @@ export class HomeComponent implements OnInit {
           return date.getMonth() === this.dateToday.getMonth() && date.getFullYear() === this.dateToday.getFullYear();
         });
         console.log(this.data);
+        this.drawLine();
       }, error => {
         console.log('Error in get Data' + error);
       }
@@ -46,15 +41,25 @@ export class HomeComponent implements OnInit {
 
     this.mesSelected = this._sharepointService.meses[this.dateToday.getMonth()];
     console.log(this.mesSelected);
+
+
   }
+
 
   ngOnInit() {
 
-
-    this.drawLine();
-
   }
 
+  mouseEnter(i) {
+    this.itemSelected = i;
+  }
+  mouseLeave() {
+    this.itemSelected = 0;
+  }
+
+  clickContent() {
+    this.contentSelected = true;
+  }
 
   drawLine() {
     // crear lista del mes con todos los dias
@@ -67,15 +72,10 @@ export class HomeComponent implements OnInit {
       };
       this.daysLine.push(item);
     }
-
-    // for (let j = 0; j <= this.data.length; j++) {
-    //   let date = new Date(this.data[j].date).getDate();
-    //   console.log(this.daysLine[date - 1]);
-    // }
-    console.log(this.data[1].date.toString());
-
-    // agregarle las noticias a ese dia del mes??
+    for (let j = 0; j < this.data.length; j++) {
+      let date = new Date(this.data[j].date).getDate();
+      this.daysLine[date - 1].notices.push(this.data[j]);
+    }
+    console.log(this.daysLine);
   }
-
-
 }
